394f69d3533be346fea694de06d8d02fd208a428
Fabricio Leitao <fabricio-beco@hotmail.com>
2020-03-27 17:58:28 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/fabricioramos/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/fabricioramos/cursomc/domain/PagamentoComBoleto.java
@@ -3,6 +3,8 @@ package com.fabricioramos.cursomc.domain;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import com.fabricioramos.cursomc.domain.enums.EstadoPagamento;
 
@@ -10,7 +12,9 @@ import com.fabricioramos.cursomc.domain.enums.EstadoPagamento;
 public class PagamentoComBoleto extends Pagamento{
 	private static final long serialVersionUID = 1L;
 	
+	@Temporal(TemporalType.DATE)
 	private Date dataVencimento;
+	@Temporal(TemporalType.DATE)
 	private Date dataPagamento;
 	
 	public PagamentoComBoleto() {
--- src/main/java/com/fabricioramos/cursomc/domain/Pedido.java
+++ src/main/java/com/fabricioramos/cursomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.fabricioramos.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,7 +12,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class Pedido implements Serializable{
@@ -19,6 +24,8 @@ public class Pedido implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date instante;
 	
 	//mapeamento um pra um
@@ -32,6 +39,9 @@ public class Pedido implements Serializable{
 	@ManyToOne
 	@JoinColumn(name="endereco_de_entrega_id")
 	private Endereco endereco;
+	
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
 	
 	public Pedido() {
 		
@@ -84,7 +94,15 @@ public class Pedido implements Serializable{
 	public void setEndereco(Endereco endereco) {
 		this.endereco = endereco;
 	}
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
 
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -109,6 +127,7 @@ public class Pedido implements Serializable{
 			return false;
 		return true;
 	}
+
 	
 	
 }
