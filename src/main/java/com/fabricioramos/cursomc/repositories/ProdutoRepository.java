344b528977e44c912da031b60e442de455ce2def
Fabricio Leitao <fabricio-beco@hotmail.com>
2020-03-30 15:08:23 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/fabricioramos/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/fabricioramos/cursomc/repositories/ProdutoRepository.java
@@ -1,12 +1,29 @@
 package com.fabricioramos.cursomc.repositories;
 
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.fabricioramos.cursomc.domain.Categoria;
 import com.fabricioramos.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 
-	
+	//Usando nomes do spring data
+	Page<Produto> findDinstinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
+		
+	//Utilizando a query com o nome data, o @Query tem prioridade. SPQL
+	/*
+	  		@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE
+			obj.nome LIKE %:nome% AND cat IN :categorias")
+			Page<Produto> findDistinctByNomeContainingAndCategoriasIn(
+			@Param("nome") String nome,
+			@Param("categorias") List<Categoria> categorias,
+			Pageable pageRequest);
+	*/
 }
---
+++ src/main/java/com/fabricioramos/cursomc/services/ProdutoService.java
@@ -0,0 +1,38 @@
+package com.fabricioramos.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.fabricioramos.cursomc.domain.Categoria;
+import com.fabricioramos.cursomc.domain.Produto;
+import com.fabricioramos.cursomc.repositories.CategoriaRepository;
+import com.fabricioramos.cursomc.repositories.ProdutoRepository;
+import com.fabricioramos.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository repo;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	public Produto find(Integer id) {
+		Optional<Produto> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repo.findDinstinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+	}
+}
