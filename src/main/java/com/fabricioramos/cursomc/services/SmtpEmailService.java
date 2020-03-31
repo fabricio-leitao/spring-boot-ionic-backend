3cc36185f7f8e94f6fce9d8b17b816f51794933d
Fabricio Leitao <fabricio-beco@hotmail.com>
2020-03-31 14:28:05 -0300

    MockEmailService com Logger. Padrões Strategy e Template Method Corrigido(desconsiderar os dois anteriores)

---
+++ src/main/java/com/fabricioramos/cursomc/services/SmtpEmailService.java
@@ -0,0 +1,13 @@
+package com.fabricioramos.cursomc.services;
+
+import org.springframework.mail.SimpleMailMessage;
+
+public class SmtpEmailService extends AbstractEmailService {
+
+	@Override
+	public void sendEmail(SimpleMailMessage msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
