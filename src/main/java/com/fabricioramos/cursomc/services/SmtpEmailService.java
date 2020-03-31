86bbd30dd52a9b29dfe976b74aabb4645c3addcc
Fabricio Leitao <fabricio-beco@hotmail.com>
2020-03-31 15:59:22 -0300

    Implementando SmtpEmailService com servidor do Google Corrigido

--- src/main/java/com/fabricioramos/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/fabricioramos/cursomc/services/SmtpEmailService.java
@@ -1,17 +1,13 @@
 package com.fabricioramos.cursomc.services;
-
-<<<<<<< HEAD
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
-=======
->>>>>>> 3cc36185f7f8e94f6fce9d8b17b816f51794933d
 import org.springframework.mail.SimpleMailMessage;
 
 public class SmtpEmailService extends AbstractEmailService {
 
-<<<<<<< HEAD
+
 	@Autowired
 	private MailSender mailSender;
 	
@@ -22,12 +18,7 @@ public class SmtpEmailService extends AbstractEmailService {
 		LOG.info("Enviando email...");
 		mailSender.send(msg);
 		LOG.info("Email enviado!");
-=======
-	@Override
-	public void sendEmail(SimpleMailMessage msg) {
-		// TODO Auto-generated method stub
-		
->>>>>>> 3cc36185f7f8e94f6fce9d8b17b816f51794933d
-	}
 
+
+}
 }
