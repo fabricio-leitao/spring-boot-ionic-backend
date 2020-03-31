594e0f910ea8a68e8d4abfd4a12ee8ab013153ee
Fabricio Leitao <fabricio-beco@hotmail.com>
2020-03-31 15:14:21 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/fabricioramos/cursomc/config/DevConfig.java
+++ src/main/java/com/fabricioramos/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.fabricioramos.cursomc.services.DBService;
+import com.fabricioramos.cursomc.services.EmailService;
+import com.fabricioramos.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +31,10 @@ public class DevConfig {
 		
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
