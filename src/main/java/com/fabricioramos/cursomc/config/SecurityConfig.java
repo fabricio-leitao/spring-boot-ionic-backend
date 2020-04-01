a3677823903cb8e9aa4d5699de40c106245e1d84
Fabricio Leitao <fabricio-beco@hotmail.com>
2020-04-01 17:04:02 -0300

    Implementando autorizacao

--- src/main/java/com/fabricioramos/cursomc/config/SecurityConfig.java
+++ src/main/java/com/fabricioramos/cursomc/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.fabricioramos.cursomc.security.JWTAuthenticationFilter;
+import com.fabricioramos.cursomc.security.JWTAuthorizationFilter;
 import com.fabricioramos.cursomc.security.JWTUtil;
 
 @Configuration
@@ -49,6 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().csrf().disable();
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
