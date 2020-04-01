f567cd327225611ce1dd4d0e2d86a57cf2a64c9c
Fabricio Leitao <fabricio-beco@hotmail.com>
2020-04-01 16:24:22 -0300

    Implementando autenticação e geração do token JWT

--- src/main/java/com/fabricioramos/cursomc/services/DBService.java
+++ src/main/java/com/fabricioramos/cursomc/services/DBService.java
@@ -123,10 +123,10 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "fabricio.joicee@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		Cliente cli1 = new Cliente(null, "Maria Silva", "fabricio.joicee@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("1235"));
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
-		Cliente cli2 = new Cliente(null, "Ana Costa", "fabrizbeco@gmail.com", "36374552377", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		Cliente cli2 = new Cliente(null, "Ana Costa", "fabrizbeco@gmail.com", "36374552377", TipoCliente.PESSOAFISICA, pe.encode("1234"));
 		cli2.getTelefones().addAll(Arrays.asList("45676333", "92567393"));
 		cli2.addPerfil(Perfil.ADMIN);
 
---
+++ src/main/java/com/fabricioramos/cursomc/services/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.fabricioramos.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.fabricioramos.cursomc.domain.Cliente;
+import com.fabricioramos.cursomc.repositories.ClienteRepository;
+import com.fabricioramos.cursomc.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if(cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+}
