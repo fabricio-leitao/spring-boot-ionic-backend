d2a5c6863a093cf89bf6d232be1281e5c461ab3d
Fabricio Leitao <fabricio-beco@hotmail.com>
2020-04-01 22:03:30 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/fabricioramos/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/fabricioramos/cursomc/repositories/PedidoRepository.java
@@ -1,12 +1,15 @@
 package com.fabricioramos.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.fabricioramos.cursomc.domain.Cliente;
 import com.fabricioramos.cursomc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
 
-	
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 }
--- src/main/java/com/fabricioramos/cursomc/services/PedidoService.java
+++ src/main/java/com/fabricioramos/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fabricioramos.cursomc.domain.Cliente;
 import com.fabricioramos.cursomc.domain.ItemPedido;
 import com.fabricioramos.cursomc.domain.PagamentoComBoleto;
 import com.fabricioramos.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.fabricioramos.cursomc.domain.enums.EstadoPagamento;
 import com.fabricioramos.cursomc.repositories.ItemPedidoRepository;
 import com.fabricioramos.cursomc.repositories.PagamentoRepository;
 import com.fabricioramos.cursomc.repositories.PedidoRepository;
+import com.fabricioramos.cursomc.security.UserSS;
+import com.fabricioramos.cursomc.services.exceptions.AuthorizationException;
 import com.fabricioramos.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -68,5 +74,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
